dddbea306103a4fb662d8d0127ddd8632fe6f758
ankur22 <akahank@gmail.com>
2020-10-25 21:36:15 +0000

    Add comments to the error pkg file

--- internal/err/error.go
+++ internal/err/error.go
@@ -5,20 +5,25 @@ import (
 	"strings"
 )
 
+// A Const error which means that the error can't be
+// changed, unlike a var error.
 type Const string
 
+// Get the error message
 func (c Const) Error() string {
 	return string(c)
 }
 
+// Is c the same as thet (target)
 func (c Const) Is(t error) bool {
 	ts := t.Error()
 	cs := c.Error()
 	return ts == cs || strings.HasPrefix(ts, cs+": ")
 }
 
-func (err Const) Wrap(inner error) error {
-	return wrapError{msg: string(err), err: inner}
+// Wrap the inner error inside the const error
+func (c Const) Wrap(inner error) error {
+	return wrapError{msg: string(c), err: inner}
 }
 
 type wrapError struct {
@@ -26,6 +31,7 @@ type wrapError struct {
 	err error
 }
 
+// Get the error message
 func (w wrapError) Error() string {
 	if w.err != nil {
 		return fmt.Sprintf("%s: %v", w.msg, w.err)
@@ -33,9 +39,12 @@ func (w wrapError) Error() string {
 	return w.msg
 }
 
-func (err wrapError) Unwrap() error {
-	return err.err
+// Unwrap the inner error
+func (w wrapError) Unwrap() error {
+	return w.err
 }
-func (err wrapError) Is(target error) bool {
-	return Const(err.msg).Is(target)
+
+// Is w the same as thet (target)
+func (w wrapError) Is(target error) bool {
+	return Const(w.msg).Is(target)
 }
