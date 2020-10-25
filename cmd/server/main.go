e07eda2f72d05b41dacdaf51f2e47edf418fde97
ankur22 <akahank@gmail.com>
2020-10-25 11:36:17 +0000

    Add version and travis ci file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,14 +4,17 @@ import (
 	"context"
 
 	"github.com/ankur22/medium-picker/internal/logging"
+	"go.uber.org/zap"
 )
+
+var Version string
 
 func main() {
 	ctx := context.Background()
 	ctx, sync := logging.NewContext(ctx)
 	defer sync()
 
-	logging.Info(ctx, "Starting medium-picker")
+	logging.Info(ctx, "Starting medium-picker", zap.String("version", Version))
 
 	logging.Info(ctx, "Shutting down medium-picker")
 }
