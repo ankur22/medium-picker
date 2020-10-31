cabe5e1628cb22a6f8059cc9a09dafd7974d9e72
ankur22 <akahank@gmail.com>
2020-10-31 19:07:04 +0000

    Deduplicate IsUser check

--- internal/rest/handler.go
+++ internal/rest/handler.go
@@ -187,13 +187,7 @@ func (h *Handler) AddMediumSource(w http.ResponseWriter, r *http.Request) {
 
 	ctx = logging.With(ctx, zap.String("userId", userID))
 
-	if ok, err := h.s.IsUser(ctx, userID); err != nil {
-		logging.Error(ctx, "Error from store")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	} else if !ok {
-		logging.Info(ctx, "UserID not found")
-		w.WriteHeader(http.StatusNotFound)
+	if err := h.isUser(ctx, userID, w); err != nil {
 		return
 	}
 
@@ -244,13 +238,7 @@ func (h *Handler) GetMediumSource(w http.ResponseWriter, r *http.Request) {
 
 	ctx = logging.With(ctx, zap.String("userId", userID))
 
-	if ok, err := h.s.IsUser(ctx, userID); err != nil {
-		logging.Error(ctx, "Error from store")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	} else if !ok {
-		logging.Info(ctx, "UserID not found")
-		w.WriteHeader(http.StatusNotFound)
+	if err := h.isUser(ctx, userID, w); err != nil {
 		return
 	}
 
@@ -310,13 +298,7 @@ func (h *Handler) DeleteMediumSource(w http.ResponseWriter, r *http.Request) {
 
 	ctx = logging.With(ctx, zap.String("userId", userID), zap.String("sourceID", sourceID))
 
-	if ok, err := h.s.IsUser(ctx, userID); err != nil {
-		logging.Error(ctx, "Error from store")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	} else if !ok {
-		logging.Info(ctx, "UserID not found")
-		w.WriteHeader(http.StatusNotFound)
+	if err := h.isUser(ctx, userID, w); err != nil {
 		return
 	}
 
@@ -330,3 +312,16 @@ func (h *Handler) DeleteMediumSource(w http.ResponseWriter, r *http.Request) {
 }
 
 var emailRegex = regexp.MustCompile(`(?:[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])`)
+
+func (h *Handler) isUser(ctx context.Context, userID string, w http.ResponseWriter) error {
+	if ok, err := h.s.IsUser(ctx, userID); err != nil {
+		logging.Error(ctx, "Error from store")
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	} else if !ok {
+		logging.Info(ctx, "UserID not found")
+		w.WriteHeader(http.StatusNotFound)
+		return errors.New("user with userID not found")
+	}
+	return nil
+}
