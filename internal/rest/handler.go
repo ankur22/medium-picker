51ea5e3bb6d316eb5a8f64d4cb4238c31ed881fd
ankur22 <akahank@gmail.com>
2020-11-04 00:17:40 +0000

    Deduplicate the writing of the list of sources

--- internal/rest/handler.go
+++ internal/rest/handler.go
@@ -267,28 +267,7 @@ func (h *Handler) GetMediumSource(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	resp := make([]pkgRest.Source, len(srcs))
-	for i, s := range srcs {
-		resp[i] = pkgRest.Source{
-			ID:  s.ID,
-			URL: s.URL,
-		}
-	}
-
-	respB, err := json.Marshal(resp)
-	if err != nil {
-		logging.Error(ctx, "failed to marshall get sources response", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(respB)
-	if err != nil {
-		logging.Error(ctx, "failed to write get sources response", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	h.writeSourceResponse(ctx, w, srcs)
 }
 
 // DeleteMediumSource deletes the source for the specified userID with sourceID
@@ -353,6 +332,25 @@ func (h *Handler) PickSources(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	h.writeSourceResponse(ctx, w, srcs)
+}
+
+var emailRegex = regexp.MustCompile(`(?:[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])`)
+
+func (h *Handler) isUser(ctx context.Context, userID string, w http.ResponseWriter) error {
+	if ok, err := h.s.IsUser(ctx, userID); err != nil {
+		logging.Error(ctx, "Error from store", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	} else if !ok {
+		logging.Info(ctx, "UserID not found")
+		w.WriteHeader(http.StatusNotFound)
+		return errors.New("user with userID not found")
+	}
+	return nil
+}
+
+func (h *Handler) writeSourceResponse(ctx context.Context, w http.ResponseWriter, srcs []store.Source) {
 	resp := make([]pkgRest.Source, len(srcs))
 	for i, s := range srcs {
 		resp[i] = pkgRest.Source{
@@ -375,19 +373,4 @@ func (h *Handler) PickSources(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-}
-
-var emailRegex = regexp.MustCompile(`(?:[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])`)
-
-func (h *Handler) isUser(ctx context.Context, userID string, w http.ResponseWriter) error {
-	if ok, err := h.s.IsUser(ctx, userID); err != nil {
-		logging.Error(ctx, "Error from store", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return err
-	} else if !ok {
-		logging.Info(ctx, "UserID not found")
-		w.WriteHeader(http.StatusNotFound)
-		return errors.New("user with userID not found")
-	}
-	return nil
 }
